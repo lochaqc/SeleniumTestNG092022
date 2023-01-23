78608008e94f986edbc661a9136b5797240cc9b3
lochaqc <lochaqc1996@gmail.com>
2023-01-23 17:48:02 +0700

    Add source code testng frameword

---
+++ src/test/java/lochaqc/TestRunXML/RunTestSample.java
@@ -0,0 +1,16 @@
+package lochaqc.TestRunXML;
+
+import org.testng.annotations.Test;
+
+public class RunTestSample {
+    @Test
+    public void firstTest() {
+        System.out.println("First test method");
+    }
+
+    @Test
+    public void secondTest() {
+        System.out.println("Second test method");
+    }
+
+}
