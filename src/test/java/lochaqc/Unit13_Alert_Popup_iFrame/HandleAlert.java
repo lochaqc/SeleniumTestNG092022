4d05d389d4953215db456ff5f7c0dd06776f2efb
lochaqc <lochaqc1996@gmail.com>
2023-01-26 11:47:31 +0700

    Update source code for Alert, Popup window and iFrame sections

---
+++ src/test/java/lochaqc/Unit13_Alert_Popup_iFrame/HandleAlert.java
@@ -0,0 +1,71 @@
+package lochaqc.Unit13_Alert_Popup_iFrame;
+
+import lochaqc.common.BaseTest;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HandleAlert extends BaseTest {
+    @Test
+    public void TestAlert01() throws InterruptedException {
+        driver.get("http://demo.guru99.com/test/delete_customer.php");
+
+        driver.findElement(By.name("cusid")).sendKeys("53920");
+        driver.findElement(By.name("submit")).submit();
+
+        // Khai báo chuyển hướng đến Alert với đối tượng
+        Alert alert = driver.switchTo().alert();
+
+        // Get message trên alert
+        String alertMessage = driver.switchTo().alert().getText();
+
+        // Displaying alert message
+        System.out.println(alertMessage);
+        Thread.sleep(2000);
+
+        // accept() = nhấn Ok button
+        // driver.switchTo().alert().accept(); //Cách 1
+        alert.accept(); //Cách 2
+
+        //Nhấn Cancel button
+        //alert.dismiss();
+
+        Thread.sleep(8000);
+
+        Alert alert2 = driver.switchTo().alert();
+
+        Thread.sleep(2000);
+
+        System.out.println(alert2.getText());
+    }
+
+    @Test
+    public void TestAlert02() throws InterruptedException {
+        driver.get("http://demo.automationtesting.in/Alerts.html");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[normalize-space()='Alert with Textbox']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[normalize-space()='click the button to demonstrate the prompt box']")).click();
+        //button[normalize-space()='click the button to demonstrate the prompt box']
+        //Nhấn sendKeys vào ô text
+        driver.switchTo().alert().sendKeys("Anh Tester");
+        Thread.sleep(2000);
+        driver.switchTo().alert().accept();
+        Thread.sleep(2000);
+        WebElement resut = driver.findElement(By.xpath("//p[@id='demo1']"));
+        System.out.println(resut.getText());
+        Assert.assertTrue(resut.getText().contains("Anh Tester"), "Value has not been filled yet");
+        Thread.sleep(2);
+    }
+
+
+
+
+}
---
+++ src/test/java/lochaqc/Unit13_Alert_Popup_iFrame/HandleIFrame.java
@@ -0,0 +1,30 @@
+package lochaqc.Unit13_Alert_Popup_iFrame;
+
+import lochaqc.common.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class HandleIFrame extends BaseTest {
+    @Test
+    public void iFrame01() throws InterruptedException {
+
+        driver.get("https://anhtester.com/contact");
+        Thread.sleep(10000);
+        System.out.println("iframe total: " + driver.findElements(By.tagName("iframe")).size());
+        Thread.sleep(1000);
+        //----Switch to content of Messenger--------
+        driver.switchTo().frame(0);
+        System.out.println(driver.findElement(By.tagName("strong")).getText());
+
+        //----Switch to icon of Messenger---------
+
+        //1. Switch to Parent WindowHandle
+        driver.switchTo().parentFrame();
+
+        //2. Switch to iframe icon of Messenger
+        driver.switchTo().frame(1);
+        driver.findElement(By.tagName("svg")).click(); //Nhấn icon để ẩn messenger chat đi
+
+        Thread.sleep(2000);
+    }
+}
